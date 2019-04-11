ed1ee157b58ce10cb1b922b7668e340ebc5effbb
Ginnie <ginnie313@DESKTOP-GQF05RL.localdomain>
2019-04-11 14:10:07 -0500

    mostly done

--- books/booksdatasourcetest.py
+++ books/booksdatasourcetest.py
@@ -1,5 +1,10 @@
 '''
-    booksdatasourcetest.py
+booksdatasourcetest.py
+4/12/19
+Kate Grossman and Ginnie White
+
+A program that contains unit tests for booksdatasource.py. Tests are broken up
+in the code based on what method they're testing.
 
 '''
 import booksdatasource
@@ -8,12 +13,14 @@ import unittest
 class BooksDataSourceTest(unittest.TestCase):
 
     def setUp(self):
-        self.books_checker = booksdatasource.BooksDataSource(books.csv, authors.csv, books_authors.csv)
+        self.books_checker = booksdatasource.BooksDataSource("books.csv", "authors.csv", "books_authors.csv")
 
     def tearDown(self):
         pass
 
-    # Book function tests
+
+    #  Book function tests
+
 
     def test_zero_book(self):
         self.assertEqual(self.books_checker.book(0), "All Clear")
@@ -27,104 +34,103 @@ class BooksDataSourceTest(unittest.TestCase):
 
     # Books function tests
 
+
     def test_invalid_id_books(self):
-        self.assertRaises(ValueError, self.books_checker.books, -1, None, None, None, 'title')
+        self.assertRaises(ValueError, self.books_checker.books, author_id=-1)
 
     def test_valid_id_books(self):
-        self.assertRaises(ValueError, self.books_checker.books, 0, None, None, None, 'title')
+        self.assertEqual(self.books_checker.books(author_id=0), ['All Clear'])
 
     def test_search_text_books(self):
-        #Tests this one parameter works for a valid case
-        self.assertEqual(self.books_checker.books(None, "Jane", None, None, None), ["Jane Eyre"])
+        self.assertEqual(self.books_checker.books(search_text="Jane"), ["Jane Eyre"])
 
-    def test_start_year_books(self):
-        #Tests this one parameter works for a valid case
-        self.assertEqual(self.books_checker.books(None, None, 2010, None, None), ["All Clear", "Blackout"])
+    def test_start_year__default_books(self):
+        self.assertEqual(self.books_checker.books(start_year=2010), ["All Clear", "Blackout"])
 
     def test_end_year_books(self):
-        self.assertEqual(self.books_checker.books(None, None, None, 1760, None), ["The Life and Opinions of Tristram Shandy, Gentleman"])
+        self.assertEqual(self.books_checker.books(end_year=1760), ["The Life and Opinions of Tristram Shandy, Gentleman"])
 
     def test_search_by_title_books(self):
-        #Tests this one parameter works for a valid case
-        self.assertEqual(self.books_checker.books(None, None, 2010, None, "title"), ["All Clear", "Blackout"])
+        self.assertEqual(self.books_checker.books(start_year=2010, sort_by="title"), ["All Clear", "Blackout"])
 
     def test_search_by_year_same_year_books(self):
-        self.assertEqual(self.books_checker.books(None, None, 2010, None, "year"), ["All Clear", "Blackout"])
-
-    def test_search_by_default_books(self): #possibly get rid of this test
-        self.assertEqual(self.books_checker.books(None, None, 2010, None, None), ["All Clear", "Blackout"])
+        self.assertEqual(self.books_checker.books(start_year=2010, sort_by="year"), ["All Clear", "Blackout"])
 
     def test_no_parameters_returns_list_books(self):
-        booksList = authors(self.books_checker.books())
+        booksList = self.books_checker.books()
         self.assertTrue(booksList, list)
 
     def test_no_parameters_returns_dictionaries_books(self):
-        booksList = authors(self.books_checker.books())
-        self.assertTrue(booksList.get(0), dictionary)
+        booksList = self.books_checker.books()
+        self.assertTrue(booksList[0], dictionary)
+        # Because books is an empty function, there is no list returned
+        # so booksList is an empty list and there's an error here
 
     def test_start_year_too_big_books(self):
-        assertEqual(self.books_checker.books(None, None, 3000, None, None), [])
-        # test that if we start at year 3000, returns empty list
+        self.assertEqual(self.books_checker.books(start_year=3000), [])
 
     def test_end_year_too_small_books(self):
-        assertEqual(self.books_checker.books(None, None, None, 0, None), [])
-        # test that if we end at year 0, returns empty list
+        self.assertEqual(self.books_checker.books(end_year=0), [])
 
     def end_year_before_start_year_books(self):
-        assertEqual(self.books_checker.books(None, None, 1800, 1700, None), [])
+        self.assertEqual(self.books_checker.books(start_year=1800, end_year=1700), [])
+
 
     # Authors function tests
 
+
     def test_invalid_id_authors(self):
-        self.assertRaises(ValueError, self.books_checker.authors, -1, None, None, None, 'birth_year')
+        self.assertRaises(ValueError, self.books_checker.authors, book_id=-1)
 
     def test_valid_id_authors(self):
-        self.assertRaises(ValueError, self.books_checker.authors, 0, None, None, None, 'title')
+        self.assertEqual(self.books_checker.authors(book_id=0),["Connie", "Willis"])
 
     def test_search_text_authors(self):
-        #Tests this one parameter works for a valid case
-        self.assertEqual(self.books_checker.authors(None, "Ton", None, None, 'title'), [["Toni", "Morrison"],["Toni", "Morrison"]])
-        pass
+        self.assertEqual(self.books_checker.authors(search_text="Ton"), [["Toni", "Morrison"],["Toni", "Morrison"]])
 
     def test_start_year_authors(self):
-        self.assertEqual(self.books_checker.authors(None, None, 1974, None, None), [["Naomi", "Alderman"]])
+        self.assertEqual(self.books_checker.authors(start_year=1976), [["Naomi", "Alderman"]])
 
     def test_end_year_authors(self):
-        self.assertEqual(self.books_checker.authors(None, None, None, 1776, None), [["Jane", "Austen"]])
+        self.assertEqual(self.books_checker.authors(end_year=1776), [["Jane", "Austen"]])
 
     def test_search_by_birth_year_authors(self):
-        self.assertEqual(self.books_checker.authors(None, None, 1945, 1947, "birth_year"), [["Salman", "Rushdie"], ["Connie", "Willis"]])
+        self.assertEqual(self.books_checker.authors(start_year=1945, end_year=1947, sort_by="birth_year"), [["Salman", "Rushdie"], ["Connie", "Willis"]])
 
     def test_search_by_default_authors(self):
-        self.assertEqual(self.books_checker.authors(None, None, 1945, 1947, None), [["Salman", "Rushdie"], ["Connie", "Willis"]])
+        self.assertEqual(self.books_checker.authors(start_year=1945, end_year=1947), [["Salman", "Rushdie"], ["Connie", "Willis"]])
 
     def test_no_parameters_returns_list_authors(self):
-        authorsList = authors(self.books_checker.authors())
+        authorsList = self.books_checker.authors()
         self.assertTrue(authorsList, list)
 
     def test_no_parameters_returns_dictionaries_authors(self):
-        authorsList = authors(self.books_checker.books())
-        self.assertTrue(authorsList.get(0), dictionary)
+        authorsList = self.books_checker.authors()
+        self.assertTrue(authorsList[0], dictionary)
+        # Because authors is an empty function, there is no list returned
+        # so authorsList is an empty list and there's an error here
 
     def test_start_year_too_big_authors(self):
-        # test that if we start at year 3000, returns empty list
-        assertEqual(self.books_checker.authors(None, None, 3000, None, None), [])
+        self.assertEqual(self.books_checker.authors(start_year=3000), [])
 
     def test_end_year_too_small_authors(self):
-        assertEqual(self.books_checker.authors(None, None, None, 0, None), [])
-        pass
+        self.assertEqual(self.books_checker.authors(end_year=0), [])
 
     def end_year_before_start_year_authors(self):
-        assertEqual(self.books_checker.authors(None, None, 1800, 1700, None), [])
-        pass
+        self.assertEqual(self.books_checker.authors(start_year=1800, end_year=1700), [])
+
 
     # Author function tests
+
 
     def test_zero_authors(self):
         self.assertEqual(self.books_checker.author(0), "Willis, Connie")
 
     def test_negative_authors(self):
-        self.assertRaises(ValueError,self.books_checker.author., -1)
+        self.assertRaises(ValueError,self.books_checker.author, -1)
 
     def test_larger_than_max_author(self):
         self.assertRaises(ValueError,self.books_checker.author, 100)
+
+if __name__ == '__main__':
+    unittest.main()
